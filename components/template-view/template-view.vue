1d0cd3b2d9f6e0a33645206a100c9703b97119ee
zhangliangming1 <zhanglm@cndatacom.com>
2022-08-14 10:16:59 +0800

    初始导入

---
+++ components/template-view/template-view.vue
@@ -0,0 +1,20 @@
+<template>
+	<view></view>
+</template>
+
+<script>
+export default {
+	name: 'template-view',
+	data() {
+		return {
+		};
+	},
+	props: {},
+	methods: {},
+	created: function() {},
+	destroyed() {},
+	mounted: function() {}
+};
+</script>
+
+<style scoped></style>
---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
